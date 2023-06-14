96dadd7ebfa5a2cd007c677bad3352b9a03ea8fa
Walid Harkous <harkouswalid@gmail.com>
2023-06-14 00:43:09 -0400

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "../styles/app.css";
+import "./styles/app.css";
+
+const img = require("./assets/images/floating-island.png");
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      Lofi Loft
+      <img src={img} alt="floating island" />
+    </div>
+  );
 }
 
 export default App;
